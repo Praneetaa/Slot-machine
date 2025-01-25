5867c2d4ee054fcf83cf61273239059442d89955
Praneeta <praneetap13@gmail.com>
2025-01-25 18:17:16 +0700

    random symbol generation

--- project.js
+++ project.js
@@ -9,6 +9,25 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8,
+}
+
+const SYMBOLS_VALUES = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2,
+}
+
+
+
 const deposit = () => {
     while (true) {
         const depositAmount = prompt("Enter a deposit amount: ");
@@ -50,6 +69,45 @@ const getBet = (balance, lines) => {
     }
 }
 
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count]  of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+
+        }
+    }
+    const reels = [];
+    for (let i = 0; i < COLS; i++){
+        reels.push([]);
+        const reelSymbols = [...symbols];   
+        for (let j = 0; j< ROWS; j++){
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length );
+            const selectedSymbol =reelSymbols [randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
+};
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++ ){
+        rows.push([]);
+        for (let j = 0; j < COLS; j++ ){
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+};
+
 let balance = deposit();
 const lines = getNumberofLines();
 const bet = getBet(balance, lines);
+const reels = spin(); 
+const rows = transpose(reels);
+
+console.log(reels);
+console.log(rows);
